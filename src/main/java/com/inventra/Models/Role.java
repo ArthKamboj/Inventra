9e2d2024684a32945a79f23fe384534e1fb22cc4
Arth Kamboj <arth23153015@akgec.ac.in>
2025-08-21 22:36:31 +0530

    User entity created

---
+++ src/main/java/com/inventra/Models/Role.java
@@ -0,0 +1,5 @@
+package com.inventra.Models;
+
+public enum Role {
+    ADMIN, USER
+}
---
+++ src/main/java/com/inventra/Models/User.java
@@ -0,0 +1,33 @@
+package com.inventra.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;  // Will be hashed
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
